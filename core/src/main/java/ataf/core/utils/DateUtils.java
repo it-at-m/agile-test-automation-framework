c2dc9f63db2433d8a490d9503c74454f26d35af2
ThomasAFink <thomas.fink@muenchen.de>
2026-07-13 15:49:53 +0200

    fix(ATAF): use birth year when computing random day of month
    LocalDate.of(yearOfBirth, monthOfBirth, 1) replaces minusYears(yearOfBirth), which wrongly treated the birth year as an offset and could produce invalid dates.

--- core/src/main/java/ataf/core/utils/DateUtils.java
+++ core/src/main/java/ataf/core/utils/DateUtils.java
@@ -124,7 +124,7 @@ public class DateUtils {
         LocalDate currentLocalDate = LocalDate.now(ZoneId.of("Europe/Berlin"));
         int yearOfBirth = currentLocalDate.getYear() - (SECURE_RANDOM.nextInt(ageMax - ageMin + 1) + ageMin);
         int monthOfBirth = SECURE_RANDOM.nextInt(12) + 1;
-        int dayOfBirth = SECURE_RANDOM.nextInt(currentLocalDate.minusYears(yearOfBirth).withMonth(monthOfBirth).lengthOfMonth()) + 1;
+        int dayOfBirth = SECURE_RANDOM.nextInt(LocalDate.of(yearOfBirth, monthOfBirth, 1).lengthOfMonth()) + 1;
         return LocalDate.parse(dayOfBirth + "." + monthOfBirth + "." + yearOfBirth, DateTimeFormatter.ofPattern("d.M.yyyy", Locale.GERMAN));
     }
 }
